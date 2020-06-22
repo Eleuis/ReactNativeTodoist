2ac42b15b762a5451e401149c9bf90fcc5b27fc7
eleuis1997 <eleuis1997@gmail.com>
2020-06-22 21:49:12 +0200

    Built sidebar component, next will be adding Firebase

---
+++ src/components/layout/Content.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+import { Header } from './Header';
+import {Sidebar} from './Sidebar';
+
+export const Content = () => (
+    <section>
+        <Sidebar />
+    </section>
+)
