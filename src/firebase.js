bf680ba627362dc25c9e1f6065f9fa97ea78a4ff
eleuis1997 <eleuis1997@gmail.com>
2020-07-01 19:58:54 +0200

    Added hook

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyCDNC1gRYe09xmQNmUqkr2jYDlW6YVWS8E",
-    authDomain: "todoist-react-native.firebaseapp.com",
-    databaseURL: "https://todoist-react-native.firebaseio.com",
-    projectId: "todoist-react-native",
-    storageBucket: "todoist-react-native.appspot.com",
-    messagingSenderId: "861880039890",
-    appId: "1:861880039890:web:3b53f563ea0b9ec0c5f00b"
+  apiKey: "AIzaSyCDNC1gRYe09xmQNmUqkr2jYDlW6YVWS8E",
+  authDomain: "todoist-react-native.firebaseapp.com",
+  databaseURL: "https://todoist-react-native.firebaseio.com",
+  projectId: "todoist-react-native",
+  storageBucket: "todoist-react-native.appspot.com",
+  messagingSenderId: "861880039890",
+  appId: "1:861880039890:web:3b53f563ea0b9ec0c5f00b",
 });
 
-export {firebaseConfig as firebase};
+export { firebaseConfig as firebase };
